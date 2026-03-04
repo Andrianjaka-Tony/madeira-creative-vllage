deda1516048015ff5cd37ed6328d8f3b59f81840
Tony <andrianjaka.tony@gmail.com>
2026-03-04 19:40:05 +0300

    home: register all new sections in home view

--- src/views/home.view.tsx
+++ src/views/home.view.tsx
@@ -3,6 +3,11 @@ import { Hero } from "@/components/home/hero";
 import { SevenDaySection } from "@/components/home/seven-day-section";
 import { ExploreSection } from "@/components/home/explore-section";
 import { TrainingSpace } from "@/components/home/traning-space";
+import { FoodSection } from "@/components/home/food-section";
+import { LocationSection } from "@/components/home/location-section";
+import { CharmSection } from "@/components/home/charm-section";
+import { TrainersSection } from "@/components/home/trainers-section";
+import { TravelSection } from "@/components/home/travel-section";
 
 export function HomeView() {
   return (
@@ -11,6 +16,11 @@ export function HomeView() {
       <SevenDaySection />
       <ExploreSection />
       <TrainingSpace />
+      <FoodSection />
+      <LocationSection />
+      <CharmSection />
+      <TrainersSection />
+      <TravelSection />
     </Fragment>
   );
 }
