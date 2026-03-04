38837901e047b34c355508f6d112ec7ba7f5c26b
Tony <andrianjaka.tony@gmail.com>
2026-03-04 13:39:26 +0300

    home: register all sections in home view

--- src/views/home.view.tsx
+++ src/views/home.view.tsx
@@ -1,10 +1,16 @@
 import { Fragment } from "react/jsx-runtime";
 import { Hero } from "@/components/home/hero";
+import { SevenDaySection } from "@/components/home/seven-day-section";
+import { ExploreSection } from "@/components/home/explore-section";
+import { TrainingSpace } from "@/components/home/traning-space";
 
 export function HomeView() {
   return (
     <Fragment>
       <Hero />
+      <SevenDaySection />
+      <ExploreSection />
+      <TrainingSpace />
     </Fragment>
   );
 }
